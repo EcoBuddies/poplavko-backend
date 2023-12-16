7eb5fff67ea9cf329fa29f1c19803743b01afc61
Amadej Šenk <guide5ever@gmail.com>
2023-12-16 01:54:07 +0100

    Added changes

---
+++ api/utils.py
@@ -0,0 +1,51 @@
+import json
+import xml.etree.ElementTree as ET
+
+def parseXML(unparsed_xml_data):
+
+    root = ET.fromstring(unparsed_xml_data)
+
+    postaje = []
+    meritve = []
+
+    for postaja in root.findall('.//postaja'):
+        sifra = int(postaja.get('sifra'))
+        ime_kratko = postaja.find('ime_kratko').text if postaja.find('ime_kratko') is not None else None
+        long = postaja.get('ge_dolzina')
+        lat = postaja.get('ge_sirina')
+        datum = postaja.find('datum').text if postaja.find('datum') is not None else None
+        pretok = postaja.find('pretok').text if postaja.find('pretok') is not None else None
+        pretok_znacilni = postaja.find('pretok_znacilni').text if postaja.find('pretok_znacilni') is not None else None
+        vodostaj = postaja.find('vodostaj').text if postaja.find('vodostaj') is not None else None
+        vodostaj_znacilni = postaja.find('vodostaj_znacilni').text if postaja.find('vodostaj_znacilni') is not None else None
+        visina_valov = postaja.find('znacilna_visina_valov').text if postaja.find('znacilna_visina_valov') is not None else float(0)
+        visina_valov_znacilni = 'visoki valovi' if visina_valov is not None and float(visina_valov) > 2.2 else None
+
+        postaja_data = {
+            'sifra': sifra,
+            'ime_kratko': ime_kratko,
+            'long': long,
+            'lat': lat,
+            'zadnja_sprememba': datum,
+            'pretok': pretok,
+            'pretok_znacilni': pretok_znacilni,
+            'vodostaj': vodostaj,
+            'vodostaj_znacilni': vodostaj_znacilni,
+            'visina_valov': visina_valov,
+            'visina_valov_znacilni': visina_valov_znacilni,
+        }
+        postaje.append(postaja_data)
+
+        meritev_data = {
+            'sifra': sifra,
+            'datum': datum,
+            'pretok_znacilni': pretok_znacilni,
+            'pretok': pretok,
+            'vodostaj': vodostaj,
+            'vodostaj_znacilni': vodostaj_znacilni,
+            'visina_valov': visina_valov,
+            'visina_valov_znacilni': visina_valov_znacilni,
+        }
+        meritve.append(meritev_data)
+    
+    return [postaje,meritve]
---
+++ api/xml-parser.py
@@ -0,0 +1,29 @@
+import requests
+from supabase import Client, create_client
+from utils import parseXML
+
+def fetch_and_insert_data():
+    # Replace with the URL of your XML file
+    response = requests.get('https://www.arso.gov.si/xml/vode/hidro_podatki_zadnji.xml')
+    data = response.content
+
+    # Replace with your Supabase URL and key
+    url = "https://hqzppnbdbfpalptrrzas.supabase.co"
+    key = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImhxenBwbmJkYmZwYWxwdHJyemFzIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MDI2NzI4NTcsImV4cCI6MjAxODI0ODg1N30.mx5rnff6qF9P6qMF7CejkL4IxijBH3dJfojrZYi2S7w"
+    client: Client = create_client(url, key) 
+    table_meritve = client.table('meritev')
+    table_postaja = client.table('postaja')
+
+    unparsed_xml_data = response.content 
+    
+    # Extract json data from XML elements
+    parsed_data = parseXML(unparsed_xml_data)
+
+    # Insert data into Supabase table and where there is a string type of column "test" insert as varchar
+    
+    table_postaja.delete().neq('sifra', 0).execute()
+    table_postaja.insert(parsed_data[0]).execute()
+    table_meritve.insert(parsed_data[1]).execute()
+
+if __name__ == '__main__':
+    fetch_and_insert_data()
